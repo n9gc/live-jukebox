46590b02528f841c88d02283ba056059aa455fd7
E0SelmY4V <yabo1887415157@qq.com>
2026-03-31 03:48:44 +0800

    add(lib): 添加了 Result 机制
    可以用来清楚的表示为什么操作失败了

--- packages/lib/jukebox/autoPicker.ts
+++ packages/lib/jukebox/autoPicker.ts
@@ -5,16 +5,26 @@
  */
 declare module 'lib/jukebox/autoPicker';
 
-import { PlayerName, Song } from 'lib/player';
+import { Song } from 'lib/player';
+import { Result } from 'lib/util';
 
 /**备选点歌器 */
-export interface AutoPicker {
-	/**获得一首备选歌 */
-	pick(this: this): Song<PlayerName> | null;
-	/**切换模式，具体怎么切换可以自定义 */
-	changeType(this: this): void;
+export abstract class AutoPicker {
 	/**歌曲列表 */
-	songs: Song<PlayerName>[];
+	songs: Song[] = [];
+	/**获得一首备选歌 */
+	abstract pick(this: this): Song | Result;
+
+	/**当前播放模式 */
+	protected abstract pickType: string;
+	/**切换模式，具体怎么切换可以自定义 */
+	changeType(this: this): void {
+		1 + 1;
+	}
+	/**获得当前模式 */
+	getPickType(this: this): string {
+		return this.pickType;
+	}
 }
 
 /**播放方式 */
@@ -35,33 +45,34 @@ const typeChangeMap: Record<PickType, PickType> = {
 	[PickType.Circular]: PickType.Random,
 };
 /**一种通用的备选点歌器 */
-export class CommonPicker implements AutoPicker, PickerMap {
+export class CommonPicker extends AutoPicker implements PickerMap {
 	/**当前点到哪了 */
 	protected index = 0;
 	[PickType.Random](this: this) {
 		return this.songs.at(this.index = Math.floor(Math.random() * this.songs.length))
-			?? null;
+			?? Result.PickNoMusic;
 	};
 	[PickType.Sequential](this: this) {
 		return this.songs.at(this.index++)
-			?? null;
+			?? this.songs.length
+			? Result.PickEnd
+			: Result.PickNoMusic;
 	};
 	[PickType.Circular](this: this) {
 		return this.songs.at(this.index++)
 			?? this.songs.at(this.index = 0)
-			?? null;
+			?? Result.PickNoMusic;
 	};
 
-	pick(this: this): Song<PlayerName> | null {
+	override pick(this: this): Song | Result {
 		return this[this.pickType]();
 	}
-	changeType(this: this): void {
+	override changeType(this: this): void {
 		this.pickType = typeChangeMap[this.pickType];
 	}
 	constructor(
-		/**播放顺序 */
-		public pickType: PickType = PickType.Circular,
-		public songs: Song<PlayerName>[] = [],
-	) { }
+		override pickType: PickType = PickType.Circular,
+		override songs: Song[] = [],
+	) { super(); }
 }
 
