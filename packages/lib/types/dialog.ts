2440a3a89b2ec1886af51234566214f5f1b4eeac
E0SelmY4V <yabo1887415157@qq.com>
2026-04-09 02:51:00 +0800

    refactor(docs, lib): 发现客户端上线时不需要发信息也能让服务端知道它上线了

--- packages/lib/types/dialog.ts
+++ packages/lib/types/dialog.ts
@@ -20,22 +20,12 @@ import z from 'zod';
 /**对话的意思 */
 export type Meaning = Enumified<typeof Meaning>;
 export namespace Meaning {
-	/**求求服务端发我歌单 */
-	export const ClientBeg = Symbol();
 	/**我已放完歌曲 */
 	export const ClientEnd = Symbol();
 	/**当前歌单是什么 */
 	export const ServerSongs = Symbol();
 	mark({ Meaning });
 }
-
-/**求求信息解析器 */
-export const DialogClientBegCodec = getJsonCodec(z.object({
-	meaning: getSymbolCodec(Meaning.ClientBeg),
-	data: z.null(),
-}));
-/**求求信息 */
-export type DialogClientBeg = z.infer<typeof DialogClientBegCodec>;
 
 /**放完歌信息解析器 */
 export const DialogClientEndCodec = getJsonCodec(z.object({
@@ -55,7 +45,6 @@ export type DialogServerSongs = z.infer<typeof DialogServerSongsCodec>;
 
 /**信息解析器 */
 export const DialogCodec = z.union([
-	DialogClientBegCodec,
 	DialogClientEndCodec,
 	DialogServerSongsCodec,
 ]);
