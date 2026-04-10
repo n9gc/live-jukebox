114c81e549da04229ac482bb05bf7d7c01f55513
E0SelmY4V <yabo1887415157@qq.com>
2026-04-11 04:28:08 +0800

    add(lib): 基本把整个点歌机都搭出来了

--- packages/lib/jukebox/index.ts
+++ packages/lib/jukebox/index.ts
@@ -10,65 +10,81 @@ export * from './config';
 export * from './parser';
 export * from './songList';
 
-import { AutoPicker, CommonPicker } from 'lib/jukebox/autoPicker';
-import { CancelMethod, SongList } from 'lib/jukebox/songList';
-import { Player, Song } from 'lib/player';
-import { Reader } from 'lib/reader';
-import { Danmaku } from 'lib/types';
-
-/**点播机配置 */
-export interface JukeboxConfig {
-	/**读评论器 */
-	readonly readers?: Reader[];
-	/**播放器 */
-	readonly players?: Player[];
-	/**备选点歌器 */
-	readonly autoPicker?: AutoPicker;
-	/**取消的方法 */
-	readonly cancelMethod?: CancelMethod;
-}
-/**默认配置 */
-export const defaultJukeboxConfig: Required<JukeboxConfig> = {
-	readers: [],
-	players: [],
-	autoPicker: new CommonPicker(),
-	cancelMethod: CancelMethod.Blocking,
-};
+import { getJukeboxConfig, JukeboxConfig } from 'lib/jukebox/config';
+import { Command, Parser } from 'lib/jukebox/parser';
+import { SongList } from 'lib/jukebox/songList';
+import { isNotOk, isOk } from 'lib/result';
+import { DialogEventer, Meaning } from 'lib/types';
+import { thr } from 'lib/util';
 
 /**点播机 */
 export class Jukebox {
 	/**配置 */
-	readonly config: Required<JukeboxConfig>;
+	protected readonly config: ReturnType<typeof getJukeboxConfig>;
+	/**歌曲列表管理器 */
 	protected readonly songList: SongList;
+	/**弹幕解析器 */
+	protected readonly parser: Parser;
 	constructor(
 		/**配置 */
 		config: JukeboxConfig,
-		/**发送要播放的歌曲信息的函数 */
-		sender: (song: Song) => void,
-		// /**发送要播放的歌曲信息的函数 */
-		// sender: (song: Song) => void,
+		/**对话 */
+		protected readonly dialogEventer: DialogEventer,
 	) {
 		const {
+			readers,
+			players,
+			distinguisher,
 			autoPicker,
 			cancelMethod,
-		} = this.config = {
-			...defaultJukeboxConfig,
-			...config,
-		};
-		this.songList = new SongList(
-			autoPicker,
-			cancelMethod,
-			sender,
-		);
+		} = this.config = getJukeboxConfig(config);
+
+		const songList = this.songList = new SongList(autoPicker, cancelMethod);
+
+		this.parser = new Parser(readers, players, distinguisher)
+			.addListener(
+				Command.Cancel,
+				this.songsAfter(picker => {
+					const r = songList.cancel(picker);
+					this.dialogEventer.dispatch(Meaning.ServerCancelResult, r);
+				}),
+			)
+			.addListener(
+				Command.Song,
+				this.songsAfter(song => {
+					const r = songList.add(song);
+					if (isNotOk(r)) thr('有重复添加的歌曲', song);
+				}),
+			);
+
+		dialogEventer
+			.addListener(
+				Meaning.ClientEnd,
+				this.songsAfter(song => {
+					const r = songList.end(song);
+					if (isOk(r)) return;
+					this.dialogEventer.dispatch(Meaning.ServerEndResult, r);
+				}),
+			);
 	}
 
-	/**获得播放器 */
-	async selectPlayer(danmaku: Danmaku): Promise<Player | null> {
-		for (const player of this.config.players) {
-			const song = await player.parse(danmaku);
-			if (song !== null) return player;
-		}
-		return null;
+	/**
+	 * 在 fn 后触发一次歌曲列表的同步
+	 * @param fn 要干的事
+	 */
+	protected songsAfter<T>(this: this, fn: (n: T) => void) {
+		return (n: T) => {
+			fn(n);
+			this.dispatchSongs();
+		};
+	}
+
+	/**手动触发一次歌曲列表的同步 */
+	dispatchSongs(this: this) {
+		this.dialogEventer.dispatch(
+			Meaning.ServerSongs,
+			this.songList.getSongs(),
+		);
 	}
 }
 
--- packages/lib/types/dialog.ts
+++ packages/lib/types/dialog.ts
@@ -5,14 +5,14 @@
  */
 declare module 'lib/types/dialog';
 
-import { ResultPick } from 'lib/result';
+import { ResultListCancel, ResultListEnd, ResultPick } from 'lib/result';
 import {
+	baseSongSchema,
 	Enumified,
 	getEnumCodec,
+	getJsonCodec,
 	getSymbolCodec,
 	mark,
-	baseSongSchema,
-	getJsonCodec,
 } from 'lib/types';
 import { Eventer } from 'lib/util';
 import z from 'zod';
@@ -24,32 +24,63 @@ export namespace Meaning {
 	export const ClientEnd = Symbol();
 	/**当前歌单是什么 */
 	export const ServerSongs = Symbol();
+	/**歌曲结束的结果 */
+	export const ServerEndResult = Symbol();
+	/**取消的结果 */
+	export const ServerCancelResult = Symbol();
 	mark({ Meaning });
 }
 
-/**放完歌信息解析器 */
-export const DialogClientEndCodec = getJsonCodec(z.object({
-	meaning: getSymbolCodec(Meaning.ClientEnd),
-	data: baseSongSchema,
-}));
+/**方便地获取序列化器 */
+function getCodec<T extends Meaning, D extends z.ZodType>(meaning: T, data: D) {
+	return getJsonCodec(z.object({
+		meaning: getSymbolCodec(meaning),
+		data,
+	}).readonly());
+}
+
 /**放完歌信息 */
-export type DialogClientEnd = z.infer<typeof DialogClientEndCodec>;
-
-/**歌单信息解析器 */
-export const DialogServerSongsCodec = getJsonCodec(z.object({
-	meaning: getSymbolCodec(Meaning.ServerSongs),
-	data: z.union([z.array(baseSongSchema), getEnumCodec(ResultPick)]),
-}));
+export type ClientEnd = z.infer<typeof ClientEnd>;
+export const ClientEnd = getCodec(
+	Meaning.ClientEnd,
+	baseSongSchema.readonly(),
+);
 /**歌单信息 */
-export type DialogServerSongs = z.infer<typeof DialogServerSongsCodec>;
+export type ServerSongs = z.infer<typeof ServerSongs>;
+export const ServerSongs = getCodec(
+	Meaning.ServerSongs,
+	z.union([
+		z.array(baseSongSchema.readonly()).readonly(),
+		getEnumCodec(ResultPick),
+	]),
+);
 
-/**信息解析器 */
-export const DialogCodec = z.union([
-	DialogClientEndCodec,
-	DialogServerSongsCodec,
-]);
+/**结束结果信息 */
+export type ServerEndResult = z.infer<typeof ServerEndResult>;
+export const ServerEndResult = getCodec(
+	Meaning.ServerEndResult,
+	getEnumCodec(ResultListEnd),
+);
+
+/**取消结果信息 */
+export type ServerCancelResult = z.infer<typeof ServerCancelResult>;
+export const ServerCancelResult = getCodec(
+	Meaning.ServerCancelResult,
+	z.union([
+		baseSongSchema.readonly(),
+		getEnumCodec(ResultListCancel),
+	]),
+);
+
+
 /**信息 */
-export type Dialog = z.infer<typeof DialogCodec>;
+export type Dialog = z.infer<typeof Dialog>;
+export const Dialog = z.union([
+	ClientEnd,
+	ServerSongs,
+	ServerEndResult,
+	ServerCancelResult,
+]);
 
 /**对话事件表 */
 export type DialogEvent = {
