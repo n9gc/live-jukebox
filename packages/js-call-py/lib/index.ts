d13ec17aff2aa7228f6793505dff2fe9726be521
E0SelmY4V <yabo1887415157@qq.com>
2026-06-09 02:48:31 +0800

    add(js-call-py): 初始化包

---
+++ packages/js-call-py/lib/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Export python function to js typesafely through rpc based on child_process and stdio
+ * @license MIT
+ * @author accurtype
+ */
+declare module 'js-call-py';
+
+export const hh = 123;
