b7b2316424294045e7eb114445a7e415cf443964
E0SelmY4V <yabo1887415157@qq.com>
2026-07-01 07:03:21 +0800

    add(js-call-py): 添加命令行主题

--- packages/js-call-py/bin/run.ts
+++ packages/js-call-py/bin/run.ts
@@ -1,8 +1,42 @@
 #!/usr/bin/env node
 
 import { execute } from '@oclif/core';
+import type { Theme } from '@oclif/core/interfaces';
+import packageInfo from '../package.json' with { type: 'json' };
+
+const theme: Theme = {
+	json: {
+		boolean: 'yellow',
+		number: 'yellowBright',
+		null: 'bold',
+		string: 'green',
+	},
+	version: 'gray',
+	command: 'cyan',
+	dollarSign: 'gray',
+	flag: 'yellow',
+	flagDefaultValue: 'green',
+	flagOptions: 'green',
+	flagSeparator: 'gray',
+};
 
 await execute({
 	dir: import.meta.url,
 	development: process.env.NODE_ENV === 'development',
+	loadOptions: {
+		root: import.meta.dirname + '/..',
+		pjson: {
+			...packageInfo,
+			oclif: {
+				commands: {
+					strategy: 'explicit',
+					target: './lib/cli-commands/index',
+				},
+				bin: 'js-call-py',
+				dirname: 'js-call-py',
+				topicSeparator: ' ',
+				theme,
+			},
+		},
+	},
 });
